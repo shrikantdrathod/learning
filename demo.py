aaeb877a6dd620922a7eb0bffd62bbae9e49a923
SHRIKANT RATHOD <70259465+shrikantdrathod@users.noreply.github.com>
2020-09-02 11:23:43 +0530

    Update demo.py
    Printing new line

--- demo.py
+++ demo.py
@@ -1,6 +1,6 @@
 def forloop(list):
     for item in list:
         if item%2==0:
-            print(item)
+            print("even item inside list: ",item)
 
 forloop([1,2,3,4,5,65,34,76,87,90,12,32,8,9,10])
