70b9def7eeb5ec83f1a362f019cfac74fa00a926
SHRIKANT RATHOD <70259465+shrikantdrathod@users.noreply.github.com>
2020-09-02 11:45:53 +0530

    added one line shrikant

--- demo.py
+++ demo.py
@@ -2,5 +2,7 @@ def forloop(list):
     for item in list:
         if item%2==0:
             print("even item inside list: ",item)
+            
+print("shrikant")
 
 forloop([1,2,3,4,5,65,34,76,87,90,12,32,8,9,10])
